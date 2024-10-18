020b2471d8bcba4fad7e5af1e618a7c5f7b5dd87
NicolasSoto02 <nicola.sotoa@duocuc.cl>
2024-10-18 10:15:32 -0300

    super usuario creado y tablas categorias y estado_ticket pobladas

--- SAS_tickets/admin.py
+++ SAS_tickets/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Usuario,Categoria,SLA,Estado_Ticket,Ticket,Mensaje,Foto_Ticket,Tipo_Sancion,Sancion,Reporte,Auditoria,Email,Log_Email
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Categoria)
+admin.site.register(SLA)
+admin.site.register(Estado_Ticket)
+admin.site.register(Ticket)
+admin.site.register(Mensaje)
+admin.site.register(Foto_Ticket)
+admin.site.register(Tipo_Sancion)
+admin.site.register(Sancion)
+admin.site.register(Reporte)
+admin.site.register(Auditoria)
+admin.site.register(Email)
+admin.site.register(Log_Email)
